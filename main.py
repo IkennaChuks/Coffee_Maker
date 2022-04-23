a15e8231b83bda898bfbf361ec11f34de258a660
Ikenna Chuks <chuks2ikenna@gmail.com>
2022-04-23 08:46:58 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,34 @@
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+menu = Menu()
+coffee_maker = CoffeeMaker()
+money_machine = MoneyMachine()
+
+
+option = menu.get_items()
+
+is_on = True
+
+
+while is_on:
+    choice = input(f'What would you like ({option}): ')
+    drink = menu.find_drink(choice)
+    if choice == 'off':
+        is_on = False
+    elif choice == 'report':
+        coffee_maker.report()
+        money_machine.report()
+    else:
+        if coffee_maker.is_resource_sufficient(drink):
+            if money_machine.make_payment(drink.cost):
+                coffee_maker.make_coffee(drink)
+
+
+
+
+
+
+
+
